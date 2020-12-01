5ed8abea36f2ad03e07380ef61482760c06d45fb
Toto Minai <bufhdy@hotmail.com>
2020-12-01 20:42:57 +0800

    Initial Commit

---
+++ DVD Player/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DVD Player
+//
+//  Created by Toto Minai on 2020/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
